f3501068515408ffbddc19bd2f688083b0021c73
wrr <1973976518@qq.com>
2020-09-03 14:44:40 +0800

    完成了后台主页面的功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,6 +1,18 @@
 //注意： 每次调用$.get() $.post() 或$.ajax的时候
 //会先调用$.ajaxPrefilter() options请求参数对象
 $.ajaxPrefilter(function(options){
-    console.log(options)
+    // console.log(options)
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+    if(options.url.indexOf('/my/') !== -1){
+        options.headers = {
+            Authorization:localStorage.getItem('token') || ''
+        }
+    }
+    //无论请求或失败都会执行complete这个函数
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+$(function () {
+    getUserInfo()
+    var layer = layui.layer
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确认退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+
+            layer.close(index);
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index)
+        });
+    })
+})
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        method: 'GET',
+
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            renderAvatar(res.data)
+        }
+
+    })
+
+}
+
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()//隐藏文字头像
+    } else {
+        $('.layui-nav-img').hide()//隐藏图像头像
+        var first = name[0].toUpperCase()//获取用户名 的第一个首字母并设置大写
+        $('.text-avatar').html(first).show()
+    }
+
+}
